93a544a5cf00d4c8e22c0fb59479502cd6288cf3
axeleroy <blackeco@free.fr>
2016-02-15 16:32:03 +0100

    Ajout des fiches de formation.

--- js/handlebars-helpers.js
+++ js/handlebars-helpers.js
@@ -1,3 +1,16 @@
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b) // Or === depending on your needs
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
+Handlebars.registerHelper("if_mod", function(index_count,mod,res, block) {
+  if(parseInt(index_count)%(mod)=== res){
+    return block.fn(this);
+  }
+});
+
 Handlebars.registerHelper('asHtml', function(item) {
     var html = "";
     _.forEach(item.value, function(value) {
